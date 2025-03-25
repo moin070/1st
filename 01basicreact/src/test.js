995d8b77f7dec96219bc42f2dc9b890808012bbe
moin khan <mkpmoinkhan@gmail.com>
2025-03-25 14:09:17 +0530

    2nd commit

---
+++ 01basicreact/src/test.js
@@ -0,0 +1,17 @@
+
+
+function Test(){
+    return(
+        <h1>testing the file which is created by me </h1>
+    )
+}
+
+
+//  2nd function is not working 
+// function Test2(){
+//     return(
+//         <h2>testing 2 function</h2>
+//     )
+// }
+
+export default Test
